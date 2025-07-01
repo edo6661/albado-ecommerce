3e877332659f61f7510d4f534d97549647023b28
edawg <muhammadridho5654@gmail.com>
2025-07-01 12:49:10 +0700

    wip: midtrans integration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
+    App\Providers\MidtransServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
     App\Providers\ServiceProvider::class,
 ];
--- routes/admin/product.php
+++ routes/admin/product.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::delete('products/bulk-destroy', [ProductController::class, 'bulkDestroy'])->name('products.bulk-destroy');
